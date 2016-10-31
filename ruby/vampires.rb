c0df97a49d607a086fa729538909e9efab0fff2f
rrichardsonv <rrichardsonv+github@gmail.com>
2016-10-31 10:18:57 -0500

    Finished functions, switching from passing multiple values to an array

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,14 @@
 #Asks employee for name, occupation, age, year of birth, garlic bread order, interested in health insurance
 
-class employee
-  def initialize (nme, job, age, year, garlic, health_insur)
+#original initialize parameters switching to an array
+#nme, job, age, year, garlic, health_insur
+class Employee
+  def initialize (*a)
+    i = 0
+#Chose count over the myriad of other options because I read ahead in the 
+#release a little. And it seemed from research to be useful. We'll find out! :)
+    surv_lengt = a.count{}
+    while surv_lengt
     @ee_nme = nme
     @ee_job = job
     @ee_age = age
@@ -12,24 +19,77 @@ class employee
 end
 
 def survey
-  puts "Welcome new employee! \n We have a quick survey for you to fill out \n (it's mandatory)"
+  result = Array.new(6)
+  puts "Welcome new employee! \n We have a quick survey for you to fill out \n (It's mandatory)"
 
-  puts "" 
+  puts "What is your name?" 
   nme = gets.chomp
-  puts ""
+  result[0] = nme
+  #return nme
+  puts "That's a nice name"
+
+  
+  puts "What is your new position here?"
   job = gets.chomp
-  puts ""
+  result[1] = job
+  #return job
+  puts "We need more of those around here!" 
+  
+  puts "How old are you?"
   age = gets.chomp
-  puts ""
-  age = age.to_i
-  puts ""
+  result[2] = age.to_i
+  #return age
+  puts "Age is just a number!"
+  
+  puts "What year were you born?"
   year = gets.chomp
-  year = year.to_i
-  puts ""
+  result[3] = year.to_i
+
+  #return year
+  puts "Best time to be alive"
+  
+  puts "Our company cafeteria serves garlic bread. \nShould we order some for you? (y/n)"
   garlic = gets.chomp
-  garlic.downcase
-  puts ""
+  garlic = garlic.downcase
+    if garlic == "y"
+        result[4] = true
+        #return garlic
+        "I know, right?"
+
+  elsif health_insur == "n"
+        result[4] = false
+        #return garlic
+        "I know, right?"
+  else
+        result[4] = nil
+        #return garlic
+        puts "Error. Please speak to your survey technician"
+
+  end
+  
+  puts "Would you like to enroll in the company’s health insurance? (y/n)"
   health_insur = gets.chomp
-  health_insur.downcase
-  return nme, job, age, year, garlic, health_insur
+  health_insur = health_insur.downcase
+  if health_insur == "y"
+    result[5] = true
+  elsif health_insur == "n"
+    result[5] = false
+  else
+    result[5] = nil
+    puts "Error. Please speak to your survey technician"
+  end
+  return result
 end
+
+
+
+
+employee1 = Employee.new (survey)
+
+puts employee1
+
+
+
+
+
+
