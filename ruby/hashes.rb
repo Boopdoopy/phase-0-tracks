4f2f456ab0cdd2e538fa89957693b3b2adc505d7
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-02 16:53:26 -0500

    Began psuedocode for appl

---
+++ ruby/hashes.rb
@@ -0,0 +1,34 @@
+# INTERIOR DESIGNER JOB APPLICATION
+
+# Name: __________________________________________
+# Address: _______________________________________
+# Email: _________________________________________
+# Phone: _________________________________________
+
+# Fave shade of blue: ____________________________
+
+# Wallpaper preferences (check any that apply):
+# ▢ Paisley
+# ▢ Chevrons
+# ▢ Photorealistic woodsy scenes (with or without squirrels)
+# ▢ Abstract woodsy scenes (no squirrels)
+
+# Ombre is:
+# ◯ Fierce
+# ◯ So last season
+# ◯ Practically medieval in its appalling irrelevance
+
+#Psuedocode
+
+# Initialize a hash
+# Prompt the designer/user for all of this information.
+# Convert any user input to the appropriate data type.
+# Print the hash back out to the screen when the designer has answered all of the questions.
+# Give the user the opportunity to update a key (no need to loop, once is fine).
+# If the designer says "none", skip it. 
+#the designer enters "decor_theme" (ex) ask for a new value and update the :decor_theme key. 
+#(Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) 
+#You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+# Print the latest version of the hash, and exit the program.
+
+
