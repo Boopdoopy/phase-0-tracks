d560933f743ec68d3b686ab5e94befb58ce43d0c
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-02 13:15:58 -0500

    Filled out methods hit a bug

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,48 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1,"o")
 # => “zoom”
 
-# "enhance".<???>
+#Ah cjust should be center
+"enhance".center(4)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".slice(0..-2)
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".(1..-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# Almost fell down a regular expression hole for this one haha
+# Welp not seeing anything apart from replace gonna do this one last
+  #quick outline
+  #using .sub which can take a regular expression
+  #identify 1 or more whitespaces
+  #replace them with 1 whitespace
+#Aha glad I came back to this!
+#Need to do more research
+#"Elementary,    my   dear        Watson!".sub(/+[" "]/g/," ") was not working
+
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# It's the specific ordinal index of that character
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
