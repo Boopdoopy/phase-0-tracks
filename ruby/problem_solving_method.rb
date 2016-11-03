7b1bc73faab313c8ab62cf63bbf622bf350bc59d
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-03 18:51:17 -0500

    end errors solved

--- ruby/problem_solving_method.rb
+++ ruby/problem_solving_method.rb
@@ -1,56 +1,57 @@
-def array_search(x, y)
-  count =0
-  thing = false
-  x.each do |found|
-    if found == y
-      p count
-      thing = true
-      count +=1
-    else
-      count +=1
-    end
+# def array_search(x, y)
+#   count =0
+#   thing = false
+#   x.each do |found|
+#     if found == y
+#       p count
+#       thing = true
+#       count +=1
+#     else
+#       count +=1
+#     end
 
-    end
-    if thing == false
-      p nil
-  end
-end
+#     end
+#     if thing == false
+#       p nil
+#   end
+# end
 
 tester= [2,35,26,34,5,457,235,46,732,434,734,]
-#Take an integer, and have an array of that integer length
+# #Take an integer, and have an array of that integer length
 
-def fibo(input_i)
-  @output_array = [0, 1]
-  until @output_array.length == input_i
-    @output_array << @output_array[-2] + @output_array[-1]
-  end
-  return @output_array
-end
+# def fibo(input_i)
+#   @output_array = [0, 1]
+#   until @output_array.length == input_i
+#     @output_array << @output_array[-2] + @output_array[-1]
+#   end
+#   return @output_array
+# end
+
+
+
 
 def integerize (array_i)
-# alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-counter = 0
-# # fully_sorted == false
-# array_i[counter-1] = string.split(//)
-array_i.each do |index|
-  array_i[counter]
-
-   while index <= array_i[counter-1] || index >= array_i[counter+1]
+  counter = 0
+  
+  array_i.each do |index|
+#index <= array_i[counter-1] ||
+      while index >= array_i[counter+1] || counter != array_i.length
         if index <= array_i[counter-1]
-         array_i[counter], array_i[counter+1] = array_i[counter+1], array_i[counter]
-        end
+          array_i[counter], array_i[counter+1] = array_i[counter+1], array_i[counter]
+        
         elsif index >= array_i[counter+1]
            array_i[counter], array_i[counter-1] = array_i[counter-1], array_i[counter]
-        end#unless???????
-   end
-
+       
+      end
+    end
       counter += 1
 
- end#do
+  end
+end
 
-end #def
 
-p intergerize(tester)g
+
+p integerize(tester)
    
 
   #get letter
@@ -79,3 +80,7 @@ p intergerize(tester)g
 #increase the counter^^
 #iterate one step of the string
 #loop again again
+# alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+# # fully_sorted == false
+# array_i[counter-1] = string.split(//)
+ #unless???????
