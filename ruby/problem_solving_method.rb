fbe2635ba05ae3d610cb0f71dbf8e56c002b9771
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-03 18:13:14 -0500

    added alphabetizing search

--- ruby/problem_solving_method.rb
+++ ruby/problem_solving_method.rb
@@ -26,3 +26,57 @@ def fibo(input_i)
   end
   return @output_array
 end
+
+def alphabetize (string)
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+counter = 0
+# fully_sorted == false
+str_array = string.split(//)
+str_array.each do |letter|
+  letter_id = alphabet.find_index(letter)
+  #issues for a and z?
+  right = str_array[counter+1]
+  left = str_array[counter-1]
+
+  right = alphabet.find_index(right)
+  left = alphabet.find_index(left)
+
+  unless letter_id >= right && letter_id <= left
+      case letter_id
+        when <= right
+          str_array[counter], str_array[counter+1] = str_array[counter+1], str_array[counter]
+        when >= left
+          str_array[counter], str_array[counter-1] = str_array[counter-1], str_array[counter]
+      end
+      counter += 1
+    end
+  end
+end
+   
+
+  #get letter
+  #find said in alphabet
+  #return the index number
+
+  #unless fully_sorted == true
+
+  #string[counter]  
+  #end
+#take a string
+#iterate through each letter of the string
+#counter/index
+
+# loop -V
+
+#compare the value of the cha to the alphabet value => index
+
+  
+
+
+#condition if +1 is less than and/or if -1 is 
+#greater than it will switch them
+#keep doing that til both +1 is greater and -1 is less
+
+#increase the counter^^
+#iterate one step of the string
+#loop again again
