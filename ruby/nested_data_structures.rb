0c0b2a81292f0447249f9ad56a99c1450ebd6843
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-04 14:24:09 -0500

    build nested data framework

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,114 @@
+#Coffee shop
+
+# airplane = {
+#     first_class: {
+#         pretty_name: 'First Class',
+#         seat_info: {
+#             total_seats: 6,
+#             seats_available: 4
+#         },
+#         meals: [
+#             "Leg of Pampered Lamb", 
+#             "Kale Salad"
+#         ]
+#     },
+#     business_class: {
+#         pretty_name: 'Business Class',
+#         seat_info: {
+#             total_seats: 12,
+#             seats_available: 9
+#         },
+#         meals: [
+#             "Sustainably Cultivated Lobster with Housemade Butter", 
+#             "Kale Salad"
+#         ]
+#     },
+#     coach: {
+#         pretty_name: 'Coach',
+#         seat_info: {
+#             total_seats: 50,
+#             seats_available: 23
+#         },
+#         meals: []
+#     }
+# }
+
+cafe = {
+  "behind bar": {
+    chalkboard: {
+      seasonal_specials: [
+      ]
+      holiday_greeting: "Happy halloween"
+      },
+    furniture: [
+      ],
+    equipment: [
+      ],
+    employees: [
+      ]
+    },
+  "main floor": {
+    sections: {
+      back_window:{
+
+      },
+      front_window:{
+
+      },
+      front_floor:{
+
+      },
+      back_floor:{
+
+      },
+      bar:{
+        seating:{
+          'n_seats':,
+          'empty_seats':
+        }
+
+      }
+    },
+    ambiance: "hip",
+    playlist: [
+      "Solange",
+      "Slayer",
+      "Beethoven",
+      "Blink-182"
+    ]
+
+    },
+  "bathroom": {
+    graffiti: [
+
+    ],
+    'out of toilet paper': true,
+    stall_quantity: "2 less than code"
+    },
+  "back room": [
+    "single mop",
+    "bags of coffee beans",
+    "spare espresso machine parts",
+    "manager's pack of cigarettes",
+    "bucket",
+    "bucket",
+    "bucket",
+    "bucket",
+    "bucket",
+    "bucket"
+    ],
+  "patio": {
+    furniture: [
+      ],
+    aspiring_novelists: {
+      
+      cigarette_brands: [
+      "American spirit's",
+      "Parliaments",
+      "Hand-rolled"
+      ]
+      },
+    people_watching: "excellent",
+    ambiance: "weather dependent"
+    }
+}
