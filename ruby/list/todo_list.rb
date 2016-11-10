1e96b6e26f1c74faf66f883b7c77c83c22f25b1b
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-10 11:25:45 -0600

    Defeated the uppercase L

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,4 +1,19 @@
 class Todolist
-  def initialize
+  def initialize(items)
+    @items = items
   end
+
+  def add_item(item)
+  end
+
+  def delete_item(item)
+  end
+
+  def get_item(index)
+  end
+
+  def get_items
+    @items
+  end
+
 end
