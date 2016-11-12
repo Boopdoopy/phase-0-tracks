af068c4c1605ef6dc0c03346ee13ed2ec3bd4639
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-12 16:13:32 -0600

    Got ahead of myself, TDD does not mean write thousands of tests

---
+++ ruby/game/guessing.rb
@@ -0,0 +1,19 @@
+class Guessing
+  # attr_reader :game, :remaining_guess
+  
+  # def initialize(word)
+  #   length = word.length
+  #   length /= 3 
+  #   length += 1
+  #   @remaining_guess = length
+  #   @game = Hash.new
+  #   @prev_guess = Array.new
+    
+  #   char_array = word.split('')
+  #     char_array.each do |char|
+  #       @game << {char, "_ "}
+  #     end
+  #   end
+
+  # end
+end
--- ruby/game/guessing_spec.rb
+++ ruby/game/guessing_spec.rb
@@ -1,43 +1,33 @@
-require "./guessing"
-describe Guessing_game do 
-  let(:hangman) {Guessing_game.new("lighting")}
+require "./Guessing"
+describe Guessing do 
+  # let(:hangman) {Guessing.new("lighting")}
 
-  it "takes in a string,calculates 1/3 + 1 of the length of the word,
-      stores the result in max guesses,splits the string given into single characters, 
-      stores each character in a hash as keys with value _ "
-    do
-    expect(hangman.game).to eq {
-      "l"=>"_ ", "i"=>"_ ",
-      "g"=>"_ ", "h"=>"_ ", 
-      "t"=>"_ ", "i"=>"_ ", 
-      "n"=>"_ ", "g"=>"_ " 
-     } 
-     expect(hangman.remaining_guess).to eq 3
-     expect(hangman.prev_guess).to eq []
-    end
+  # it "takes in a string,stores max guesses, stores each character in a hash as keys with value _ " do
+  #   # expect(hangman.game).to equal 
+  #   #   {:l=>"_ ", :i=>"_ ",
+  #   #   :g=>"_ ", :h=>"_ ", 
+  #   #   :t=>"_ ", :i=>"_ ", 
+  #   #   :n=>"_ ", :g=>"_ "} 
+  #    expect(hangman.remaining_guess).to eql 3
+  #    expect(hangman.prev_guess).to equal []
+  #   end
 
-  it "Returns the values from the hidden word and remaining guesses
-      as string while false, and print result when true"
-       
-    do
-    expect(hangman.reveal.to eq "_ _ _ _ _ _ _ _ \n3 guesses left"
-    end
+  # it "Returns the values from the hidden word and remaining guesses as string while false, and print result when true" do
+  #   expect(hangman.reveal.to eq "_ _ _ _ _ _ _ _ \n3 guesses left"
+  #   end
 
-  it "determines whether the game is over and the result"
-    do
-    expect(hangman.result).to eq false
-    end
-  it "takes in a single character checks it against
-      guesses, hidden word, and updates as needed"
-    do
-    hangman.guess("l")
-    expect(hangman.prev_guess).to eq ["l"]
-    expect(hangman.remaining_guess).to eq 3
-    expect(hangman.game).to eq {
-      "l"=>"l ", "i"=>"_ ",
-      "g"=>"_ ", "h"=>"_ ", 
-      "t"=>"_ ", "i"=>"_ ", 
-      "n"=>"_ ", "g"=>"_ " 
-     } 
-    end
+  # it "determines whether the game is over and the result" do
+  #   expect(hangman.result).to eq false
+  #   end
+  # it "takes in a single character checks it against guesses, hidden word, and updates as needed" do
+  #   hangman.guess("l")
+  #   expect(hangman.prev_guess).to eq ["l"]
+  #   expect(hangman.remaining_guess).to eq 3
+  #   # expect(hangman.game).to eq {
+  #   #   :l=>"l ", :i=>"_ ",
+  #   #   :g=>"_ ", :h=>"_ ", 
+  #   #   :t=>"_ ", :i=>"_ ", 
+  #   #   :n=>"_ ", :g=>"_ "
+  #   #  } 
+  #   end
 end
