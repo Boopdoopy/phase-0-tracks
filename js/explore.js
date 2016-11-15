33959038efa526a61edb6de76116c0790ce9e5bb
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-15 14:37:00 -0600

    Added string reversing function

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+// Declare function
+// Takes in a string and only a string
+// Loop through string
+// Removing each letter from the front of string
+// Store each letter at front of string
+// Return the result
+
+function strReverse (str="") {
+  var strResult = ""
+  for (var i=str.length;i>=0;i--) {
+    strResult += str.charAt(i);
+  }
+
+return strResult
+}
+
+strReverse("Woot!");
