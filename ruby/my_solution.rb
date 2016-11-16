871be6b7b6aa1d972b1431257439c777cb936a23
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-15 21:25:17 -0600

    Finished through release 7

--- ruby/my_solution.rb
+++ ruby/my_solution.rb
@@ -16,32 +16,37 @@ class VirusPredictor
  
  # run speed_of_spread and predicted_deaths
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    # (@population_density, @population, @state)
+    speed_of_spread
+    # (@population_density, @state)
   end
   private #private is protected, and you can't use it outside of the class
  
   #the method is returning the number of deaths in each state.
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
+    # (population_density, population, state)
     # predicted deaths is solely based on population density
+    multiplier = 0.0
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      multiplier = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      multiplier = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      multiplier =  0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      multiplier = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      multiplier = 0.05
     end
+    number_of_deaths = (@population_density * multiplier).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
   end
   #get the speed of the spread, based on the population density
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread(speed = 0.0)
+    #   (population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
     if @population_density >= 200
       speed += 0.5
     elsif @population_density >= 150
@@ -54,20 +59,28 @@ class VirusPredictor
       speed += 2.5
     end
     #change it to p if want to see something returned.
-    puts " and will spread across the state in #{speed} months.\n\n"
+    print " and will spread across the state in #{speed} months.\n"
   end
 end
 #=======================================================================
 # DRIVER CODE
  # initialize VirusPredictor for each state
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+print "============================<CDC REPORT>===========================\n"
+STATE_DATA.each do |state,pop_info|
+  state_instance = VirusPredictor.new(state, pop_info[:population_density],pop_info[:population])
+  print "___________________________________________________________________\n"
+  state_instance.virus_effects
+end
+
+
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 #=======================================================================
 # Reflection Section
 #What kind of variable is STATE_DATA: global variable, and what's its scope?
