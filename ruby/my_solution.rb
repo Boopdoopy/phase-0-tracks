aa3ac911123ed920f41cc69d4b63365f56a8b5b5
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-15 20:38:02 -0600

    Was passed the driver role

--- ruby/my_solution.rb
+++ ruby/my_solution.rb
@@ -1,28 +1,27 @@
 # Virus Predictor
-
 # I worked on this challenge [by myself, with: ].
 # We spent [#] hours on this challenge.
-
 # EXPLANATION OF require_relative
-#
-#
+#require_relative vs require
+#require will just look in the load path. A global variable that ruby has access to.
+#require_relative will be searched in the current folder that you call out. more predictable.
 require_relative 'state_data'
-
 class VirusPredictor
-
+ #initialize instance variables, only list out parameters in initialize.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+ 
+ # run speed_of_spread and predicted_deaths
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
-
-  private
-
+  private #private is protected, and you can't use it outside of the class
+ 
+  #the method is returning the number of deaths in each state.
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -36,16 +35,13 @@ class VirusPredictor
     else
       number_of_deaths = (@population * 0.05).floor
     end
-
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
   end
-
+  #get the speed of the spread, based on the population density
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
-
     if @population_density >= 200
       speed += 0.5
     elsif @population_density >= 150
@@ -57,31 +53,21 @@ class VirusPredictor
     else
       speed += 2.5
     end
-
+    #change it to p if want to see something returned.
     puts " and will spread across the state in #{speed} months.\n\n"
-
   end
-
 end
-
 #=======================================================================
-
 # DRIVER CODE
  # initialize VirusPredictor for each state
-
-
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
-
 jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
 jersey.virus_effects
-
 california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
 california.virus_effects
-
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
-
-
 #=======================================================================
 # Reflection Section
+#What kind of variable is STATE_DATA: global variable, and what's its scope?
