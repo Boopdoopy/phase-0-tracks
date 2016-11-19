e7557b3b433cef6b758499d185e1c11aca7daa34
rrichardsonv <rrichardsonv+github@gmail.com>
2016-11-19 12:19:03 -0600

    Added car constructor functions

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,6 +4,7 @@ var colors = [
 "yellow",
 "red"
 ];
+
 var horseName = [
 "Ed",
 "Larry",
@@ -19,9 +20,64 @@ for (var i = 0;i<horseName.length;i++) {
   stable[name] = color;
 }
 
+//Driver Code ==================================
 console.log(stable.Curly);
 // colors.push("purple");
 // horseName.push("Johanna");
 
 // console.log(colors);
 // console.log(horseName);
+//===============================================
+
+function Car(year,type,color,isFast) {
+  console.log("Our new car:", this);
+  this.year = year;
+  this.type = type;
+  this.color = color;
+  this.isFast = isFast;
+
+  this.testDrive = function(){
+    console.log("Vroom Vroom");
+    }
+  };
+
+carNames = [
+"Jeep",
+"Stationwagon",
+"Sports Car",
+"Hummer"
+];
+
+console.log("Let's build a car!");
+var jeep = new Car(2016,"Jeep","White",true);
+console.log(jeep);
+jeep.testDrive();
+
+
+var volkswagon = new Car(1989,"Stationwagon","yellow",false);
+var ferrari = new Car(2012,"Sports car","red",true);
+var h3 = new Car(2010,"Hummer","black",true);
+
+console.log("Something family friendly!");
+console.log(volkswagon);
+console.log("Something cool!");
+console.log(ferrari);
+console.log("Something lame!");
+console.log(h3);
+
+
+// var car1
+// var car2
+// var car3
+// var car4
+
+// for (i=0;i<carNames.length;i++) {
+  
+// }
+
+
+
+
+
+
+
